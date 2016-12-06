77aaa1ec1611d83504981db09200c3f7c6c25d2f
Robert Hilton <robert.a.hilton.jr@gmail.com>
2016-12-06 00:32:18 -0500

    Update

--- Java/KnightFury_V1/src/com/team195/frc2016/Utilities/Setpoints.java
+++
@@ -1,7 +0,0 @@
-package com.team195.frc2016.Utilities;
-
-public class Setpoints {
-	public static int armSetpoint = 0;
-	public static int winchSetpoint = 0;
-	public static int tensionerSetpoint = 0;
-}
